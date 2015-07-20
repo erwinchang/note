1b1abd26a7784e7fad9f922b5cbd34ae8be7e2d8
Erwin Chang <erwin.chang@fuho.com.tw>
2015-07-20 09:47:46 +0800

    fix wget set-env.sh

--- env_tools/docker-script/hisi/install.sh
+++ env_tools/docker-script/hisi/install.sh
@@ -6,7 +6,7 @@ mkdir -p ./env_tools
 wget --no-check-certificate https://raw.githubusercontent.com/erwinchang/note/master/env_tools/docker-script/functions.sh -O ./env_tools/functions.sh
 chmod +x ./env_tools/function.sh
 
-wget --no-check-certificate https://github.com/erwinchang/note/blob/master/env_tools/docker-script/set-env.sh -O ./env_tools/set-env.sh
+wget --no-check-certificate https://raw.githubusercontent.com/erwinchang/note/master/env_tools/docker-script/set-env.sh -O ./env_tools/set-env.sh
 chmod +x ./env_tools/set-env.sh
 
 cd ./env_tools
