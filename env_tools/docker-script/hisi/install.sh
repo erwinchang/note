3d41bf6525afc79941d4387227433700a3fe1d24
Erwin Chang <erwin.chang@fuho.com.tw>
2015-07-20 09:24:26 +0800

    fix wget fail

--- env_tools/docker-script/hisi/install.sh
+++ env_tools/docker-script/hisi/install.sh
@@ -3,10 +3,10 @@
 #
 
 mkdir -p ./env_tools
-wget https://raw.githubusercontent.com/erwinchang/note/master/env_tools/docker-script/functions.sh -O ./env_tools/functions.sh
+wget --no-check-certificate https://raw.githubusercontent.com/erwinchang/note/master/env_tools/docker-script/functions.sh -O ./env_tools/functions.sh
 chmod +x ./env_tools/function.sh
 
-wget https://github.com/erwinchang/note/blob/master/env_tools/docker-script/set-env.sh -O ./env_tools/set-env.sh
+wget --no-check-certificate https://github.com/erwinchang/note/blob/master/env_tools/docker-script/set-env.sh -O ./env_tools/set-env.sh
 chmod +x ./env_tools/set-env.sh
 
 cd ./env_tools
